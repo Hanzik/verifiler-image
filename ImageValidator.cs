00e5bae7da66b0f5842d01911e37965690877b64
Honza Vrátník <vratnja2@fel.cvut.cz>
2017-04-24 00:16:10 +0200

    Repository initialization

---
+++ ImageValidator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using NLog;
+using VerifilerCore;
+
+namespace VerifilerImage {
+	/// <summary>
+	/// This validation step tries to load the image file via built-in Bitmap class.
+	/// 
+	/// The error code produced by this validation is Error.Corrupted.
+	/// </summary>
+	public class ImageValidator : FormatSpecificValidator {
+
+		public override int ErrorCode { get; set; } = Error.Corrupted;
+
+		public override void Setup() {
+			Name = "Image .jpg, .png files verification";
+			RelevantExtensions.Add(".jpg");
+			RelevantExtensions.Add(".png");
+			Enable();
+		}
+
+		public override void ValidateFile(string file) {
+			try {
+				Bitmap image = new Bitmap(file);
+			} catch (Exception e) {
+				ReportAsError("File is corrupted: " + file + "; Message: " + e.Message);
+			}
+		}
+	}
+}
